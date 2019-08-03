5c92b3f7eac975d0a0e842b6dab0a44da9692bbf
Jason Howie <howiejason93@gmail.com>
2019-08-03 17:19:58 -0400

    change gitignore

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Nav from './components/Nav';
 import PhotoContainer from './components/PhotoContainer';
 import './css/index.css';
 
+// import apiKey from './config';
+
 class App extends Component {
 
   render() {
